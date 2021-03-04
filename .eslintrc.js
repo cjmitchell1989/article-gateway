3c348f559df46f978236f364fa18ea23bcb7e7e4
Chris Mitchell <cjmitchell1989@gmail.com>
2021-03-04 10:19:25 +0000

    Added linting

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es6': true,
+    'mocha': true
+  },
+  'extends': [
+    'eslint:recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2, {
+        'SwitchCase': 1
+      }
+    ],
+    'quotes': [
+      'error',
+      'single',
+      {
+        'allowTemplateLiterals': true
+      }
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-var': [
+      'error'
+    ],
+    'eqeqeq': [
+      'error'
+    ],
+    'no-debugger': [
+      'error'
+    ],
+    'keyword-spacing': [
+      'error'
+    ],
+    'space-before-blocks': [
+      'error'
+    ],
+    'require-atomic-updates': [
+      'off'
+    ]
+  }
+}
